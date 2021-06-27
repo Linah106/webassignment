aa36361a32876fea88fd090e730018eee2e58f63
Alfeo Raymond <ngelime07@gmail.com>
2021-06-27 19:44:47 +0300

    more

--- login.php
+++ login.php
@@ -25,8 +25,65 @@ if(isset($_POST['register'])){
 
 }
 
-include('navbar.php');
 
+?>
+<?php
+if(isset($_SESSION['user'])){ ?>
+    <!Doctype html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>My Layout</title>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="form.css">
+
+  </head>
+  <body>
+    <div class="main">
+  <div class="header">
+      <h1 style="color:orange;">CV Store</h1>
+          <ul>
+            <li><a href="index.php" class="active">Home</a></li>
+
+            <li><a href="course.php" class="active">Courses</a></li>
+            <li><a href="about.php" class="active">CV</a></li>
+            <li><a href="logout.php">Logout</a></li>
+
+
+            </li>
+          </ul>
+  </div>
+<? 
+}else{
+?>
+<!Doctype html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>My Layout</title>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="form.css">
+
+  </head>
+  <body>
+    <div class="main">
+  <div class="header">
+  <h1 style="color:orange;">CV Store</h1>
+          <ul>
+          <li><a href="about.php" class="active">About</a></li>
+          <li><a href="contact.php">Contact</a></li>
+
+            <li><a href="register.php">Register</a></li>
+              <li><a href="login.php">Login</a></li>
+
+
+            </li>
+          </ul>
+  </div>
+  <?php
+}
 ?>
         <div class="welc">
 
